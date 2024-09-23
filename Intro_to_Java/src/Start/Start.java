61bb399e32ba21120b0f833007ab758b1e53aaf3
Mahir-CSE <mahirahmed.cse@gmail.com>
2024-09-23 09:09:16 +0600

    Updated Java

---
+++ Intro_to_Java/src/Start/Start.java
@@ -0,0 +1,9 @@
+package Start;
+
+public class Start {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.print("I am Mahir Ahmed\n");
+        System.out.println("I am from Dhaka, Bangladesh");
+    }
+}
